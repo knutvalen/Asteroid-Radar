396e21ba45ff52b352d0eb381b2e8d84d383587e
Knut Valen <knut.valen@icloud.com>
2020-12-10 11:10:08 +0100

    Update AsteroidsAdapter.kt

--- app/src/main/java/com/udacity/asteroidradar/main/AsteroidsAdapter.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/AsteroidsAdapter.kt
@@ -1,5 +1,33 @@
 package com.udacity.asteroidradar.main
 
-class AsteroidsAdapter {
-    
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.udacity.asteroidradar.domainmodels.Asteroid
+
+class AsteroidsAdapter() : ListAdapter<Asteroid, AsteroidsViewHolder>(DiffCallback) {
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Asteroid>() {
+        override fun areItemsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun areContentsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AsteroidsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: AsteroidsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
