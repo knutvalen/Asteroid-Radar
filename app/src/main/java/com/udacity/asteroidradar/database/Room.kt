e13e9ce7aff79b99d66d76451ae323debe2a1c85
Knut Valen <knut.valen@icloud.com>
2020-12-10 13:42:18 +0100

    Create Room.kt

---
+++ app/src/main/java/com/udacity/asteroidradar/database/Room.kt
@@ -0,0 +1,33 @@
+package com.udacity.asteroidradar.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface DataAccessObject {
+
+    @Query("SELECT * FROM DatabaseAsteroid")
+    fun getAsteroids(): LiveData<List<DatabaseAsteroid>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg asteroids: DatabaseAsteroid)
+
+}
+
+abstract class Database: RoomDatabase() {
+    abstract val dataAccessObject: DataAccessObject
+}
+
+private lateinit var INSTANCE: Database
+
+fun getDatabase(context: Context): Database {
+    synchronized(Database::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            val builder = Room.databaseBuilder(context.applicationContext, Database::class.java, "asteroids")
+            INSTANCE = builder.build()
+        }
+    }
+
+    return INSTANCE
+}
